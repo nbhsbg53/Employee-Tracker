906219c48f36bb297a81ff6121fb8a804990e7e4
Brandon Grady <nbhsbg53@gmail.com>
2023-03-05 11:45:13 -0500

    index

--- index.js
+++ index.js
@@ -5,17 +5,6 @@ const { allowedNodeEnvironmentFlags, exit, listenerCount } = require('process');
 
 const startTrack = () => {
     console.log("Welcome to Employee Tracker!");
-    console.log("|==============================================|");
-    console.log("|   ____                                       |");  
-    console.log("|  |       _   _   _      __        __  __     |");   
-    console.log("|  |---   | | | | |D)|   |  | |__| |_  |_      |");  
-    console.log("|  |____  |  V  | |  |__ |__|   |  |__ |__     |");  
-    console.log("|                                              |");  
-    console.log("|  _______                                     |");  
-    console.log("|     |     _    _    __        __   _         |");  
-    console.log("|     |    |D)  (_)  |    |_/  |_   |D)        |");  
-    console.log("|     |    | | |   | |__  | |  |__  | |        |");  
-    console.log("|==============================================|");
     firstQuestion();
 };
 
